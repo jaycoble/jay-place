ab9eb908250d787f78c12be87964a050e7c84fae
Jay <37637038+jaycoble@users.noreply.github.com>
2021-11-17 12:13:27 -0500

    going to try to deploy

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const express = require('express');
 const cloudinary = require('cloudinary');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello, world!').end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+});
+// [END gae_node_request_example]
+
+module.exports = app;
