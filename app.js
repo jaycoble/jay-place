d3d70c8d2261fb312e554a7e4f27400342ee06a6
Jay <37637038+jaycoble@users.noreply.github.com>
2021-11-18 06:54:10 -0500

    updating js, adding npm packages

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const express = require('express');
+const session = require('express-session');
+const ExpressError = require('./utilities/ExpressError');
+const ejsMate = require('ejs-mate');
+
 const cloudinary = require('cloudinary');
+const helmet = require('helmet');
 
 const app = express();
 
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(helmet());
+
+const secret = process.env.SECRET;
+
 app.get('/', (req, res) => {
-    res.status(200).send('Hello, world!').end();
-});
+    res.render('index')
+})
 
 // Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
     console.log('Press Ctrl+C to quit.');
 });
-// [END gae_node_request_example]
 
 module.exports = app;
